baa07e0e05771b091d134554967f3a498aa17dfd
coldoon11 <a.o.varlacov@mail.ru>
2026-09-15 14:15:36 +0400

    Use stable Android API 36

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.autonomousai"
-    compileSdk = 37
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.example.autonomousai"
