8e94e108a4adf4e2963015c60c37c8ef9f9cc914
coldoon11 <a.o.varlacov@mail.ru>
2026-09-15 14:10:47 +0400

    Add gesture modes

---
+++ app/src/main/java/com/example/autonomousai/GestureMode.kt
@@ -0,0 +1,10 @@
+package com.example.autonomousai
+
+enum class GestureMode(val shortLabel: String) {
+    MOVE("MOVE"),
+    PINCH("TAP"),
+    DRAG("DRAG"),
+    SCROLL("SCROLL"),
+    LONG_PRESS("MENU"),
+    PAUSED("PAUSE"),
+}
